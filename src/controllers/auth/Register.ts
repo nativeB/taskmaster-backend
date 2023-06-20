b741bb4de19f8caaa6834fb257f36027386ceb09
quincyhut <113361385+quincyhut@users.noreply.github.com>
2023-06-20 15:32:31 +0000

    complete register route

--- src/controllers/auth/Register.ts
+++ src/controllers/auth/Register.ts
@@ -1,9 +1,20 @@
+import User from "../../model/user";
+import responses from "../../utils/responses";
 
 class RegisterController {
-	public static run (req, res): any {
-		res.json({
-		result: "register",
-		});
+	public static async run (req, res): Promise<void> {
+		try{
+			const user = new User(req.body);
+			await user.save();
+			res.json({
+				message: responses.userCreatedSuccessfully,
+				user: {
+					email: user.email
+				}
+			});
+		} catch (error) {
+			res.status(500).json({ errors: error });
+		}
 	}
 }
 
--- src/utils/responses.ts
+++ src/utils/responses.ts
@@ -1,5 +1,6 @@
 export default {
     "emailIsNotValid": "Email is not valid",
     "passwordMustBeAtLeast6CharactersLong": "Password must be at least 6 characters long",
-    "passwordsDoNotMatch": "Passwords do not match"
+    "passwordsDoNotMatch": "Passwords do not match",
+    "userCreatedSuccessfully": "User created successfully",
 }
