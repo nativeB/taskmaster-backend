739f906d5ed3114c7fd3e50682dc8dcb53160431
quincyhut <113361385+quincyhut@users.noreply.github.com>
2023-06-20 11:10:19 +0000

    add in password middleware

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,8 +1,11 @@
+import * as bcrypt from "bcrypt-nodejs";
 
 import { IUser } from "../interfaces/model/users";
 import mongoose from "../providers/database";
 
-export interface IUserModel extends IUser, mongoose.Document {}
+export interface IUserModel extends IUser, mongoose.Document {
+	comparePassword(password: string, cb: any): string;
+}
 
 export const UserSchema = new mongoose.Schema<IUserModel>({
 	email: { type: String, unique: true },
@@ -13,6 +16,35 @@ export const UserSchema = new mongoose.Schema<IUserModel>({
 }, {
 	timestamps: true
 });
+
+UserSchema.pre<IUserModel>("save", function (cb) {
+	if (!this.isModified("password")) {
+		return cb();
+	}
+
+	bcrypt.genSalt(10, (err, salt) => {
+		if (err) {
+			return cb(err);
+		}
+
+		bcrypt.hash(this.password, salt, null, (err, hash) => {
+			if (err) {
+				return cb(err);
+			}
+
+			this.password = hash;
+			return cb();
+		});
+	});
+});
+
+
+// password check
+UserSchema.methods.comparePassword = function (requestPassword, cb): any {
+	bcrypt.compare(requestPassword, this.password, (err, isMatch) => {
+		return cb(err, isMatch);
+	});
+};
 
 const User = mongoose.model<IUserModel>("User", UserSchema);
 
